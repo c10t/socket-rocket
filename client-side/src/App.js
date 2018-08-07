5e1e77f314308b0635755d1e7fc048356ae5511e
c10t <c10t@fa.ke>
2018-08-07 09:32:07 +0900

    add socketio client

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import socketIoClient from 'socket.io-client';
+
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+      endpoint: "http://127.0.0.1:4001"
+    };
+  }
+
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIoClient(endpoint);
+    socket.on("FromAPI", data => this.setState({ response: data }));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div style={{ textAlign: "center" }}>
+          { response ? <p>The response is: {response} </p> 
+            : <p>Now Loading!</p>}
+        </div>
       </div>
     );
   }
